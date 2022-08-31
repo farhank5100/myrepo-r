c4defba0306bb9404ca06cddc6fa3492ed157586
Farhan Khalid <farhan.awan5100@gmail.com>
2022-08-31 17:27:17 +0500

    few line added

--- myrepo.r
+++ myrepo.r
@@ -1,3 +1,9 @@
 # my first repo
 print("my first repo")
+print("hello from github")
 
+name = "Farhan Khalid"
+version = "Rstudio - in writing"
+
+print(name)
+print("these lines has been executed")
