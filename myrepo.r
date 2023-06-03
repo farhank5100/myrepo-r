06fbd28489e2341981d967c12d86e7c87618019a
Farhan Khalid <farhan.awan5100@gmail.com>
2023-06-03 12:56:08 +0500

    new sheet added

--- myrepo.r
+++ myrepo.r
@@ -15,3 +15,25 @@ Age = 20
 last_name= "khaid"
 print(last_name)
 print(Age)
+
+x <- 0:6
+class(x)
+as.numeric(x)    # as.() function is used to change type of a object
+as.logical(x)
+as.character(x)
+as.complex(x)
+
+
+y = c("a","b","c")
+as.numeric(y)
+as.logical(y)
+
+
+m <- matrix(6:12, nrow = 2, ncol = 3)
+print(m)
+
+dim(m)     # dim() dim function is used to define or check dimensions of matrix, which is created. 
+attributes(m)
+
+dim(m) <- c(3,2)  # in matrix first value will be rows and 2nd will he column.
+m
